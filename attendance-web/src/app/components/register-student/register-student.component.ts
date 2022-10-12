a9f09b9f91e68545b7b0fecb578db5b2d059b35d
Jordan Powers <jordanpowers1227@gmail.com>
2022-10-11 20:48:55 -0700

    [Web] Clear validation errors after register student

--- attendance-web/src/app/components/register-student/register-student.component.ts
+++ attendance-web/src/app/components/register-student/register-student.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AsyncValidatorFn } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, AsyncValidatorFn, FormGroupDirective } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StudentsService } from 'src/app/services/students.service';
 import { Student } from 'src/app/models/student.model';
@@ -14,6 +14,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class RegisterStudentComponent implements OnInit {
 
   private students = new AsyncSubject<Array<Student>>();
+  private recentlyAddedStudents : Array<Student> = [];
 
   constructor(private studentsService : StudentsService, private snackBar: MatSnackBar) { }
 
@@ -24,7 +25,8 @@ export class RegisterStudentComponent implements OnInit {
   private nameTakenValidator : AsyncValidatorFn = (control) => {
     const name = (control.value ?? "").toLocaleLowerCase();
     return this.students.pipe(map(students => {
-      const invalid = students.some(student => student.name.toLocaleLowerCase() == name);
+      const allStudents = students.concat(this.recentlyAddedStudents);
+      const invalid = allStudents.some(student => student.name.toLocaleLowerCase() == name);
       if(invalid) {
         return { nameTaken: { value: control.value } };
       } else {
@@ -40,12 +42,14 @@ export class RegisterStudentComponent implements OnInit {
     })
   });
 
-  onSubmit(): void {
+  onSubmit(formData: any, formDirective: FormGroupDirective): void {
     this.mainForm.disable();
     const studentName = this.mainForm.value.name!;
     this.studentsService.registerNewStudent(studentName).subscribe((student: Student) => {
-      this.mainForm.controls.name.reset();
+      this.recentlyAddedStudents.push(student);
       this.mainForm.enable();
+      this.mainForm.reset();
+      formDirective.resetForm();
       this.snackBar.open("Student " + studentName + " registered!", '', {
         duration: 4000
       });
