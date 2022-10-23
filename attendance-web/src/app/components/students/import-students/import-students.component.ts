c1deab4a72ceac4a5bf5fc901eba43b9aee0a266
Jordan Powers <jordanpowers1227@gmail.com>
2022-10-23 14:25:20 -0700

    [Web] Start on import-students

--- attendance-web/src/app/components/students/import-students/import-students.component.ts
+++ attendance-web/src/app/components/students/import-students/import-students.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-import-students',
   templateUrl: './import-students.component.html',
@@ -10,6 +9,21 @@ export class ImportStudentsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFileSelected(event: any) {
+    console.log(event.target.files);
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      let data: string = e.target.result;
+      if(data.startsWith("\xEF\xBB\xBF")) {
+        data = data.substring(3);
+      }
+      console.log(data);
+    };
+
+    reader.readAsBinaryString(event.target.files![0]);
   }
 
 }
