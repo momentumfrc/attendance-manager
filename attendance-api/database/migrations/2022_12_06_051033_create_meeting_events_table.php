cc72262a4736756dad06656a39fed9b67bd4077f
Jordan Powers <jordanpowers1227@gmail.com>
2022-12-05 21:33:22 -0800

    [Api] Add m/r/c for MeetingEvents

---
+++ attendance-api/database/migrations/2022_12_06_051033_create_meeting_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeetingEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meeting_events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('registered_by')->constrained('users')->onDelete('restrict');
+            $table->string('type', 24);
+        });
+
+        if(config('app.debug', false)) {
+            // This integrity check adds overhead to registering new meeting events, so only add it for debugging
+            DB::statement("ALTER TABLE meeting_events ADD CONSTRAINT chk_type CHECK (type IN ('end-of-meeting'));");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meeting_events');
+    }
+}
--- attendance-api/setup.sh
+++ attendance-api/setup.sh
@@ -2,7 +2,7 @@
 
 pushd $(dirname $(readlink -f $0))
 
-docker run -it --rm -v ${PWD}/:/opt -w /opt laravelsail/php74-composer:latest bash -c "composer install"
+docker run -it --rm -v ${PWD}/:/opt -w /opt -u 1000:1000 laravelsail/php74-composer:latest bash -c "composer install"
 
 ./vendor/bin/sail up -d
 sleep 5
