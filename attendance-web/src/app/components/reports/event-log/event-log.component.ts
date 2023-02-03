1170f1c8e8258ddbac1de58bec41efe838f006ef
Jordan Powers <jordanpowers1227@gmail.com>
2023-02-03 13:01:26 -0800

    [Web] Include meeting events in event log

--- attendance-web/src/app/components/reports/event-log/event-log.component.ts
+++ attendance-web/src/app/components/reports/event-log/event-log.component.ts
@@ -3,18 +3,22 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
 import { DateTime } from 'luxon';
-import { BehaviorSubject, combineLatest, filter, map, Observable, ReplaySubject, startWith } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, forkJoin, map, Observable, ReplaySubject, startWith } from 'rxjs';
 import { AttendanceEvent } from 'src/app/models/attendance-event.model';
+import { MeetingEvent } from 'src/app/models/meeting-event.model';
 import { Student } from 'src/app/models/student.model';
 import { User } from 'src/app/models/user.model';
 import { AttendanceService } from 'src/app/services/attendance.service';
+import { MeetingsService } from 'src/app/services/meetings.service';
 import { StudentsService } from 'src/app/services/students.service';
 import { UsersService } from 'src/app/services/users.service';
 
-interface RichAttendanceEvent {
-  event: AttendanceEvent,
-  student: Student,
-  registrar: User
+interface EventLogEvent {
+  eventId: string,
+  student?: Student,
+  registrar: User,
+  event_type: string,
+  date: DateTime
 };
 
 enum PageState {
@@ -22,16 +26,16 @@ enum PageState {
   LOADED
 }
 
-class EventDataSource implements DataSource<RichAttendanceEvent> {
-  private data: Array<RichAttendanceEvent> = [];
+class EventDataSource implements DataSource<EventLogEvent> {
+  private data: Array<EventLogEvent> = [];
 
   public readonly pageSizeOptions = [25, 50, 100];
 
-  private paginatedData = new ReplaySubject<Array<RichAttendanceEvent>>(1);
+  private paginatedData = new ReplaySubject<Array<EventLogEvent>>(1);
 
   private lastPageSize = this.pageSizeOptions[0];
 
-  public setData(data: Array<RichAttendanceEvent>): void {
+  public setData(data: Array<EventLogEvent>): void {
     this.data = data;
     this.updateSlice(0, this.lastPageSize);
   }
@@ -49,11 +53,16 @@ class EventDataSource implements DataSource<RichAttendanceEvent> {
     this.paginatedData.next(this.data.slice(startIdx, endIdx));
   }
 
-  connect(collectionViewer: CollectionViewer): Observable<readonly RichAttendanceEvent[]> {
+  connect(collectionViewer: CollectionViewer): Observable<readonly EventLogEvent[]> {
     return this.paginatedData;
   }
 
   disconnect(collectionViewer: CollectionViewer): void {}
+}
+
+interface AttendanceAndMeetingData {
+  attendance: Array<AttendanceEvent>,
+  meetings: Array<MeetingEvent>
 }
 
 @Component({
@@ -74,13 +83,14 @@ export class EventLogComponent implements OnInit {
 
   eventColumns = ["eventId", "studentId", "registrarId", "eventType", "eventDate"];
 
-  events = new ReplaySubject<Array<AttendanceEvent>>(1);
+  events = new ReplaySubject<AttendanceAndMeetingData>(1);
   richEvents = new EventDataSource();
 
   constructor(
     private attendanceService: AttendanceService,
     private studentsService: StudentsService,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private meetingsService: MeetingsService
   ) {
     this.listOptions.controls['until'].valueChanges.pipe(
       filter(it => it),
@@ -89,7 +99,10 @@ export class EventLogComponent implements OnInit {
       map(dates =>({since: dates.since, until: dates.until.plus({days: 1})}))
     ).subscribe(dates => {
       this.state.next(PageState.LOADING);
-      this.attendanceService.getEvents(dates).subscribe(events => {
+      forkJoin({
+        attendance: this.attendanceService.getEvents(dates),
+        meetings: this.meetingsService.getEvents(dates)
+      }).subscribe(events => {
         this.events.next(events);
       })
     })
@@ -107,12 +120,26 @@ export class EventLogComponent implements OnInit {
         map(users => new Map(users.map(user => [user.id, user])))
       )
     }).pipe(
-        map(({events, students, users}) => events.map(event => ({
-          event: event,
-          student: students.get(event.student_id),
-          registrar: users.get(event.registered_by)
-        } as RichAttendanceEvent))
-      )
+        map(({events, students, users}) => {
+          let formattedAttendanceEvents: Array<EventLogEvent> = events.attendance.map(it => ({
+            eventId: "a-" + it.id,
+            student: students.get(it.student_id),
+            registrar: users.get(it.registered_by)!!,
+            event_type: it.type,
+            date: it.created_at
+          }));
+          let formattedMeetingEvents: Array<EventLogEvent> = events.meetings.map(it => ({
+            eventId: "m-" + it.id,
+            student: undefined,
+            registrar: users.get(it.registered_by)!!,
+            event_type: it.type,
+            date: it.created_at
+          }));
+
+          let out = formattedAttendanceEvents.concat(formattedMeetingEvents);
+          out.sort((a, b) => b.date.toMillis() - a.date.toMillis());
+          return out;
+        })
     ).subscribe((events) => {
       this.richEvents.setData(events);
     })
