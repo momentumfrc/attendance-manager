4a4014cf46552b35bff1e4f1e717f2f53359585c
Jordan Powers <jordanpowers1227@gmail.com>
2023-05-04 12:44:48 -0700

    [Web] Remember last poll time

--- attendance-web/src/app/services/poll.service.ts
+++ attendance-web/src/app/services/poll.service.ts
@@ -3,13 +3,37 @@ import { Injectable } from '@angular/core';
 import { DateTime } from 'luxon';
 import { Student } from '../models/student.model';
 import { MeetingEvent } from '../models/meeting-event.model';
-import { Observable, interval, switchMap } from 'rxjs';
+import { Observable, interval, switchMap, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 export interface PollResponse {
   updated_students: Student[],
   meeting_events: MeetingEvent[]
 };
+
+class Poller {
+  private lastUpdate: DateTime|null = null;
+
+  public constructor(
+    private pollService: PollService
+  ) {}
+
+  private getSince(): DateTime {
+    if(this.lastUpdate == null) {
+      return DateTime.now().minus({milliseconds: (1000 + environment.pollInterval)});
+    } else {
+      return this.lastUpdate.minus({milliseconds: 1000});
+    }
+  }
+
+  public getObservable(): Observable<PollResponse> {
+    return interval(environment.pollInterval).pipe(
+      switchMap(() => this.pollService.getUpdates({since: this.getSince()}).pipe(tap(() => {
+        this.lastUpdate = DateTime.now();
+      })))
+    );
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +42,7 @@ export class PollService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private getUpdates(options: {since: DateTime, until?: DateTime}): Observable<PollResponse> {
+  public getUpdates(options: {since: DateTime, until?: DateTime}): Observable<PollResponse> {
     let params = new HttpParams();
     params = params.set('since', Math.floor(options.since.toMillis() / 1000));
     if(options.until) {
@@ -29,11 +53,6 @@ export class PollService {
   }
 
   public getPollingObservable(): Observable<PollResponse> {
-    return interval(environment.pollInterval).pipe(
-      switchMap(() => {
-        const since = DateTime.now().minus({milliseconds: (1000 + environment.pollInterval)});
-        return this.getUpdates({since});
-      })
-    );
+    return new Poller(this).getObservable();
   }
 }
