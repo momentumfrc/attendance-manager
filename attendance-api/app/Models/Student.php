ae35aaa66a85e59028e476c89bda627291f04c1f
Jordan Powers <jordanpowers1227@gmail.com>
2023-09-07 18:38:00 +0000

    [Api] Soft delete students

--- attendance-api/app/Models/Student.php
+++ attendance-api/app/Models/Student.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $appends = ['last_check_in', 'last_check_out'];
     protected $fillable = ['name'];
