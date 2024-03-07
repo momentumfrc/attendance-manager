9846a8def5f8dc77f22c1d5919f3af4a932a3711
Jordan Powers <jordanpowers1227@gmail.com>
2024-03-07 00:15:42 -0800

    [Web] Add button to clear search box

--- attendance-web/src/app/components/reuse/search-box/search-box.component.ts
+++ attendance-web/src/app/components/reuse/search-box/search-box.component.ts
@@ -1,25 +1,43 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, startWith } from 'rxjs';
+import { BehaviorSubject, Subscription, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
   @Output() searchUpdatedEvent = new EventEmitter<string>();
+
+  shouldShowCancel = new BehaviorSubject<boolean>(false);
+
+  private searchUpdatedSub!: Subscription
+  private shouldShowCancelSub!: Subscription
 
   searchControl = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
-    this.searchControl.valueChanges.pipe(
+    this.searchUpdatedSub = this.searchControl.valueChanges.pipe(
       startWith(""),
       map(search => search ?? "")
     ).subscribe(this.searchUpdatedEvent);
+
+    this.shouldShowCancelSub = this.searchControl.valueChanges.pipe(
+      map(it => !!it)
+    ).subscribe(this.shouldShowCancel);
+  }
+
+  ngOnDestroy(): void {
+    this.searchUpdatedSub.unsubscribe();
+    this.shouldShowCancelSub.unsubscribe();
+  }
+
+  clearSearch() {
+    this.searchControl.setValue("");
   }
 
 }
