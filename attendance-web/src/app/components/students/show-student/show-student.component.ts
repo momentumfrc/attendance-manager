ef0209dbecf55d7f158e4fe81f791053c6fc38b7
Jordan Powers <jordanpowers1227@gmail.com>
2025-10-10 15:49:53 -0700

    [Web] Add spinner while loading student details

--- attendance-web/src/app/components/students/show-student/show-student.component.ts
+++ attendance-web/src/app/components/students/show-student/show-student.component.ts
@@ -1,23 +1,19 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DateTime } from 'luxon';
 import { BehaviorSubject, combineLatest, filter, map, Observable, of, ReplaySubject, shareReplay, startWith, Subject, switchMap, takeUntil } from 'rxjs';
+import { SelectedDateRange } from 'src/app/components/reuse/date-picker/date-picker.component';
 import { AttendanceSession } from 'src/app/models/attendance-session.model';
-import { MeetingAttendance, MeetingStudentCount } from 'src/app/models/report-models';
+import { MeetingStudentCount } from 'src/app/models/report-models';
 import { Student } from 'src/app/models/student.model';
 import { User } from 'src/app/models/user.model';
 import { AttendanceService } from 'src/app/services/attendance.service';
-import { AuthService } from 'src/app/services/auth.service';
-import { ErrorService } from 'src/app/services/error.service';
 import { PermissionsService } from 'src/app/services/permissions.service';
 import { ReportsService } from 'src/app/services/reports.service';
 import { StudentsService } from 'src/app/services/students.service';
 import { UsersService } from 'src/app/services/users.service';
 import { PaginatedDataSource } from 'src/app/utils/PaginatedDataSource';
 import { environment } from 'src/environments/environment';
-import { SelectedDateRange } from 'src/app/components/reuse/date-picker/date-picker.component';
 
 enum PageState {
   STUDENT_LOADING = 1,
@@ -86,11 +82,11 @@ export class ShowStudentComponent implements OnInit, OnDestroy {
   protected student = new ReplaySubject<Student>(1)
   protected registeredBy = new ReplaySubject<User>(1)
 
-  protected sessionsSubject = new ReplaySubject<AttendanceSession[]>(1);
+  protected sessionsSubject = new ReplaySubject<AttendanceSession[]|null>(1);
   protected meetingsSubject = new ReplaySubject<MeetingStudentCount[]>(1);
 
   protected attendanceSessions = new PaginatedDataSource<RichAttendanceSession>()
-  protected attendanceStats = new ReplaySubject<AttendanceStats>(1)
+  protected attendanceStats = new ReplaySubject<AttendanceStats|null>(1)
 
   protected dateRangeChanges = new Subject<SelectedDateRange>();
 
@@ -103,7 +99,6 @@ export class ShowStudentComponent implements OnInit, OnDestroy {
     route: ActivatedRoute,
     private usersService: UsersService,
     private attendanceService: AttendanceService,
-    private errorService: ErrorService,
     private permissionsService: PermissionsService,
     private reportsService: ReportsService
   ) {
@@ -146,10 +141,11 @@ export class ShowStudentComponent implements OnInit, OnDestroy {
         forStudentId: student.id,
         since: dateRange.since,
         until: dateRange.until
-      }))
+      }).pipe(startWith(null)))
     ).subscribe(sessions => this.sessionsSubject.next(sessions));
 
     this.sessionsSubject.pipe(
+      filter(it => it != null),
       map(sessions => sessions.map(session => new RichAttendanceSession(session))),
     ).subscribe((sessions) => {
       this.attendanceSessions.setData(sessions);
@@ -163,7 +159,7 @@ export class ShowStudentComponent implements OnInit, OnDestroy {
       sessions: this.sessionsSubject,
       meetings: this.meetingsSubject
     }).pipe(
-      map(({sessions, meetings}) => new AttendanceStats(sessions, meetings))
+      map(({sessions, meetings}) => sessions ? new AttendanceStats(sessions, meetings) : null)
     ).subscribe(this.attendanceStats);
   }
 
